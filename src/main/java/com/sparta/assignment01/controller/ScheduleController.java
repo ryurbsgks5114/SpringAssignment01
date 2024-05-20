bfb6756103b3f4fa2e0255c4529bf40aed0a5e58
ryurbsgks5114 <ryurbsgks5114@naver.com>
2024-05-21 02:05:58 +0900

    feat: level - 7
    - Swagger
      - Swagger UI를 통해 API 목록을 확인할 수 있다.
      - Swagger UI를 통해 API 테스트를 할 수 있다.
    - 유효성 검사
      - 할일 제목은 최대 200자 이내로 제한, 필수값 처리
      - 비밀번호는 필수값 처리
      - 담당자는 이메일 형식을 갖도록 처리

--- src/main/java/com/sparta/assignment01/controller/ScheduleController.java
+++ src/main/java/com/sparta/assignment01/controller/ScheduleController.java
@@ -3,6 +3,9 @@ package com.sparta.assignment01.controller;
 import com.sparta.assignment01.dto.ScheduleRequestDto;
 import com.sparta.assignment01.dto.ScheduleResponseDto;
 import com.sparta.assignment01.service.ScheduleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,31 +14,37 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Schedules API", description = "Schedules 추가, 선택 조회, 전체 조회, 수정, 삭제 API ")
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
     @PostMapping("/schedules")
-    public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleRequestDto requestDto) {
+    @Operation(description = "일정 추가")
+    public ResponseEntity<ScheduleResponseDto> createSchedule(@Valid @RequestBody ScheduleRequestDto requestDto) {
         return scheduleService.createSchedule(requestDto);
     }
 
     @GetMapping("/schedules/{id}")
+    @Operation(description = "선택 일정 조회")
     public ResponseEntity<ScheduleResponseDto> getSchedule(@PathVariable int id) {
         return scheduleService.getSchedule(id);
     }
 
     @GetMapping("/schedules")
+    @Operation(description = "전체 일정 조회")
     public ResponseEntity<List<ScheduleResponseDto>> getAllSchedules() {
         return scheduleService.getAllSchedules();
     }
 
     @PatchMapping("/schedules/{id}")
+    @Operation(description = "일정 수정")
     public ResponseEntity<ScheduleResponseDto> updateSchedule(@PathVariable int id, @RequestBody ScheduleRequestDto requestDto) {
         return scheduleService.updateSchedule(id, requestDto);
     }
 
     @DeleteMapping("/schedules/{id}")
+    @Operation(description = "일정 삭제")
     public ResponseEntity<Void> deleteSchedule(@PathVariable int id, @RequestBody ScheduleRequestDto requestDto) {
         return scheduleService.deleteSchedule(id, requestDto);
     }
--- src/main/java/com/sparta/assignment01/dto/ScheduleRequestDto.java
+++ src/main/java/com/sparta/assignment01/dto/ScheduleRequestDto.java
@@ -1,5 +1,8 @@
 package com.sparta.assignment01.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,9 +10,16 @@ import lombok.Setter;
 @Setter
 public class ScheduleRequestDto {
 
+    @NotBlank
+    @Size(max = 200)
     private String title;
+
     private String content;
+
+    @Email
     private String manager;
+
+    @NotBlank
     private String pw;
 
 }
